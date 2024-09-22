a975c1678c5fa69e55428a33832d01c8c479c145
Yash Deshmukh <46190818+deshmukh1332@users.noreply.github.com>
2024-09-22 14:20:34 +0530

    not hit missile on self

--- src/main/java/com/example/lld/machine_coding/phonepe/Battlefield.java
+++ src/main/java/com/example/lld/machine_coding/phonepe/Battlefield.java
@@ -47,8 +47,8 @@ public class Battlefield {
     }
 
     public void display() {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+        for (int i = size - 1; i >= 0; i--) {
+            for (int j = size - 1; j >= 0; j--) {
                 System.out.print(grid[i][j] + " ");
             }
             System.out.println();
--- src/main/java/com/example/lld/machine_coding/phonepe/RandomCoordinateStrategy.java
+++ src/main/java/com/example/lld/machine_coding/phonepe/RandomCoordinateStrategy.java
@@ -6,12 +6,13 @@ public class RandomCoordinateStrategy implements CoordinateStrategy {
     @Override
     public Coordinate getCoordinate(Battlefield battlefield, Player player) {
         Random random = new Random();
-        Coordinate coord;
+        Coordinate coord = null;
+        Coordinate finalCoord = coord;
         do {
             int x = random.nextInt(battlefield.size);
             int y = random.nextInt(battlefield.size);
             coord = new Coordinate(x, y);
-        } while (player.enemyMissiles.contains(coord));
+        } while (player.enemyMissiles.contains(coord) && player.ships.stream().noneMatch(ship -> ship.coordinate.contains(finalCoord)));
         player.enemyMissiles.add(coord);
         return coord;
     }
